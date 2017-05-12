c4d02c2883e1b086c2999592a9230693484574a6
adventurebear <suzanneatkinson@gmail.com>
2017-05-12 18:55:48 -0400

    able to login, but not get to redirect URL

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 // include the libraries we need
 var request = require('request');
 var cheerio = require('cheerio');
+var md5 = require('md5');
+var dotenv = require('dotenv').config()
 
 req = request.defaults({
   jar: true,                 // save cookies to jar
@@ -12,39 +14,77 @@ req = request.defaults({
   followAllRedirects: true   // allow redirections
 });
 
+var username = process.env.username
+var password = process.env.password
+
+console.log(username, password)
+
+console.log(md5(password))
 var postOptions = { method: 'POST',
   url: 'http://dev.totalimmersionacademy.com/members/login.php',
-  qs: { do: 'login' },
+  qs: { do: 'login'},
+
   headers:
-    { 'postman-token': '66c40a42-246f-e50e-c11a-51ac0c705ac4',
+    {
       'cache-control': 'no-cache',
-      cookie: '_jsuid=656517229; no_trackyy_100677842=1; _ga=GA1.2.194027884.1494441457; _gid=GA1.2.78311076.1494449044; __utmt_UA-45928938-1=1; _first_pageview=1; bb_sessionhash=2864237aabc55363612f3f6e7aac0f94; bb_lastvisit=1494451593; bb_lastactivity=0; bb_np_notices_displayed=2; _vwo_uuid_v2=0D76F6A278D917293736DEDBA85AF772|312a803ee880c0efadffc0b204f7f075; __utma=1.194027884.1494441457.1494441457.1494448543.2; __utmb=1.30.10.1494448543; __utmc=1; __utmz=1.1494441457.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __ar_v4=LSRIDWDWMVAD3KHL6GHZNL%3A20170509%3A38%7C3BCD3BD7M5C3FPS3CM5544%3A20170509%3A40%7C2W4JLZS6EVHTXO5NBA4VXX%3A20170509%3A40%7CKI43T6TGRRELVDNG2DEWHX%3A20170509%3A2; _eventqueue=%7B%22heatmap%22%3A%5B%7B%22type%22%3A%22heatmap%22%2C%22href%22%3A%22%2Fmembers%2Flogin.php%22%2C%22x%22%3A1101%2C%22y%22%3A70%2C%22w%22%3A1208%7D%5D%2C%22events%22%3A%5B%5D%7D',
       'accept-language': 'en-US,en;q=0.8',
       'accept-encoding': 'gzip, deflate',
-      referer: 'http://dev.totalimmersionacademy.com/members/login.php',
+      referer: 'http://dev.totalimmersionacademy.com/members/forum.php',
       accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
       'content-type': 'application/x-www-form-urlencoded',
       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
       'upgrade-insecure-requests': '1',
       origin: 'http://dev.totalimmersionacademy.com' },
-  form: {} };
+  formData:
+    { do: 'login',
+      s: '',
+      securitytoken: 'guest',
+      vb_login_md5password: md5(password),
+      vb_login_md5password_utf: md5(password),
+      vb_login_password: '',
+      vb_login_password_hint: 'Password',
+      vb_login_username: 'Suzanne Atkinson' } ,
+  gzip: true
+};
 
-var getOptions = {
-  method: 'GET',
-  url: 'http://dev.totalimmersionacademy.com/members/forum.php?s=9a478caa7fafff589735bf6410c87d80'
-}
-
-
-req(postOptions, function (error, response, body) {
+req(postOptions, function (error, response, html) {
   if (error) throw new Error(error);
-  console.log("Post Response: ", response);
-  //console.log(body);
 
-  req(getOptions, function (err, resp, body) {
-    console.log("Get Body: ", body);
+  var $ = cheerio.load(html)
+
+  //am I logged in?
+  var m = $('.blockrow.restore').text()
+  console.log(m)
+
+  //parse session URL from the body using jquery/cheerio
+  var url = $('noscript meta').attr('content').split("; ")[1]
+  console.log(url)
+
+  //get session ID from the URL using regex
+  var sessionUrl = url.match(/s=([^&]*)/)[1]
+  console.log("session regex:", sessionUrl)
+
+
+  //build new options object for next request (redirect)
+  var getOptions = { method: 'GET',
+    url: 'http://dev.totalimmersionacademy.com/members/forum.php',
+    qs: { s: sessionUrl },
+    headers:
+      { 'cache-control': 'no-cache',
+        sessionurl: 's=' + sessionUrl + '&' } ,
+    gzip: true
+
+  }
+
+  //check what I built
+  console.log(getOptions)
+
+  //go for the money here
+  req(getOptions,
+    function(err,resp,body){
+
+     //console.log("Get Body: ", body);
 
   })
-
-
 
 });
